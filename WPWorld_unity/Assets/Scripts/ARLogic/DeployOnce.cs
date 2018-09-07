05a9e76e3bc534414a0a03388109a0cc35c9dc6f
Ryane <ryanlua5566@gmail.com>
2018-09-07 16:47:36 +0800

    Finish the ARCore intergration, now can only deploy the item once

--- WPWorld_unity/Assets/Scripts/ARLogic/DeployOnce.cs
+++ WPWorld_unity/Assets/Scripts/ARLogic/DeployOnce.cs
@@ -24,19 +24,26 @@ public class DeployOnce : MonoBehaviour
     /// <summary>
     /// A list of planes ARCore
     /// </summary>
-    private List<DetectedPlane> _AllPlanes = new List<DetectedPlane>();
+    private List<DetectedPlane> AllPlanes = new List<DetectedPlane>();
 
     /// <summary>
-    /// Update is called once per frame
+    /// Summoning prefab
     /// </summary>
+    private GameObject prefab;
+
+    /// <summary>
+    /// Checking for prefab summoned
+    /// </summary>
+    private bool isPrefabSpawned = false;
+
     void Update()
     {
         // Tracks if there is a plane to spawn
-        Session.GetTrackables<DetectedPlane>(_AllPlanes);
+        Session.GetTrackables<DetectedPlane>(AllPlanes);
         bool _isTracked = true;
-        for (int i = 0; i < _AllPlanes.Count; i++)
+        for (int i = 0; i < AllPlanes.Count; i++)
         {
-            if (_AllPlanes[i].TrackingState == TrackingState.Tracking)
+            if (AllPlanes[i].TrackingState == TrackingState.Tracking)
             {
                 _isTracked = false;
                 break;
@@ -58,29 +65,38 @@ public class DeployOnce : MonoBehaviour
 
         // Draw a line out from the player touch postion to the surface of the real world
         if (Frame.Raycast(_touch.position.x, _touch.position.y, _raycastFilter, out _hit))
+        {      
+            if(!isPrefabSpawned)
+            {
+                // Check if it the raycast is hitting the back of the plane 
+                if ((_hit.Trackable is DetectedPlane) && Vector3.Dot(MainCamera.transform.position - _hit.Pose.position, _hit.Pose.rotation * Vector3.up) < 0)
+                {
+                    Debug.Log("Hit at back of the current DetectedPlane");
+                }
+                else
+                {                   
+                    // Its a ground plane soo doesnt matter if its a point or a plane
+                    GameObject _prefab = GroundPlanePrefab;
+
+                    // Instantiate the object at where it is hit
+                    var _GroundObject = Instantiate(_prefab, _hit.Pose.position, _hit.Pose.rotation);
+             
+
+                    // Create an anchor for ARCore to track the point of the real world
+                    var _anchor = _hit.Trackable.CreateAnchor(_hit.Pose);
+
+                    // Make the ground object the child of the anchor
+                    _GroundObject.transform.parent = _anchor.transform;
+
+                    prefab = _prefab;
+                    isPrefabSpawned = true;
+                }
+            }        
+        }
+
+        if (!prefab.activeSelf || prefab == null)
         {
-            // Check if it the raycast is hitting the back of the plane 
-            if ((_hit.Trackable is DetectedPlane) && Vector3.Dot(MainCamera.transform.position - _hit.Pose.position, _hit.Pose.rotation * Vector3.up) < 0)
-            {
-                Debug.Log("Hit at back of the current DetectedPlane");
-            }
-            else
-            {
-                // Its a ground plane soo doesnt matter if its a point or a plane
-                GameObject _prefab = GroundPlanePrefab;
-
-                // Instantiate the object at where it is hit
-                var _GroundObject = Instantiate(_prefab, _hit.Pose.position, _hit.Pose.rotation);
-
-                //_GroundObject.transform.Rotate(0, 180, 0, Space.Self);
-
-                // Create an anchor for ARCore to track the point of the real world
-                var _anchor = _hit.Trackable.CreateAnchor(_hit.Pose);
-
-                // Make the ground object the child of the anchor
-                _GroundObject.transform.parent = _anchor.transform;
-
-            }
+            isPrefabSpawned = false;
         }
     }
 }
