59052794ee79cf6c7138e21536bb36e0373f8e8e
Ryane <ryanlua5566@gmail.com>
2018-09-10 12:30:04 +0800

    Added Blinking text script and a new scene for ui testing

---
+++ WPWorld_unity/Assets/Scripts/UI/BlinkingText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkingText : MonoBehaviour {
+
+    private Text _text;
+    private const int _MAX_CD = 5;
+    private float _curr_CD;
+
+    void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if(_curr_CD > _MAX_CD)
+        {
+            _text.enabled = !_text.enabled;
+            _curr_CD = 0;
+        }
+        _curr_CD += 0.1f;
+    }
+}
