fca6d15d229601aedc86c75765ee4e0ac3f7434f
LilF <wilfredmaple@gmail.com>
2018-09-10 15:02:26 +0800

    added change mat script when collision, use it on the mystery blocks or something

---
+++ WPWorld_unity/Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour {
+    public Material[] material;
+    Renderer rend;
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            rend.sharedMaterial = material[1];
+        }
+    }
+}
