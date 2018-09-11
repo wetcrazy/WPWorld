47838b8b3692b3f6e21eba294b0541bcdd39df37
LilF <wilfredmaple@gmail.com>
2018-09-11 15:49:21 +0800

    "Teleport" Script, updating of scene

---
+++ WPWorld_unity/Assets/Scripts/PushYouAway.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushYouAway : MonoBehaviour {
+    Vector3 direction = new Vector3(0, 0, 90);
+    float pushspeed = 2.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Capsule")
+        {
+            Debug.Log("GG");
+            collision.transform.Translate(pushspeed,0,0);
+        }
+    }
+}
