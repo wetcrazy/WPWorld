93d66c69e5999a568056228e879d02b1b9480ce4
CynicalSteve <syong789@hotmail.com>
2018-09-13 14:19:31 +0800

    -Timer implemented into Asteroid run

--- WPWorld_unity/Assets/Scripts/AsteroidRun/SceneControlFinal.cs
+++ WPWorld_unity/Assets/Scripts/AsteroidRun/SceneControlFinal.cs
@@ -15,21 +15,27 @@ public class SceneControlFinal : MonoBehaviour {
     float MinimumAsteroidDistanceToPlanet;
     [SerializeField]
     float MaximumAsteroidDistanceToPlanet;
+    [SerializeField]
+    GameObject CanvasTimer;
 
     int TimerMinute = 0, TimerSecond = 0;
+    float SecondTimer = 0;
+    bool TimerIsCountingDown = true;
 
     List<GameObject> AsteroidList = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
-        string StartingTime = GameObject.Find("Timer").GetComponent<Text>().text;
+        string StartingTime = CanvasTimer.GetComponent<Text>().text;
         TimerMinute = int.Parse(StartingTime.Substring(0, 2));
-        TimerSecond = int.Parse(StartingTime.Substring(2));
+        TimerSecond = int.Parse(StartingTime.Substring(3));
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        UpdateCanvasTimer();
+
         //If there are fewer asteroids active in scene than specified, spawn an asteroid
         while (AsteroidList.Count < MaximumAsteroidsInScene)
         {
@@ -49,6 +55,86 @@ public class SceneControlFinal : MonoBehaviour {
             asteroid.transform.position += (PlanetObject.transform.position - asteroid.transform.position).normalized * asteroid.GetComponent<AsteroidScript>().AsteroidSpeed * Time.deltaTime;
         }
 	}
+
+    void UpdateCanvasTimer()
+    {
+        if (TimerSecond == 0 && TimerMinute == 0 && TimerIsCountingDown)
+        {
+            //Timer has reached 0
+            TimerIsCountingDown = false;
+        }
+
+        //Updates the timer on the UI canvas
+        SecondTimer += Time.deltaTime;
+
+        //If 1 second has passed
+        if (SecondTimer >= 1)
+        {
+            string temptime = CanvasTimer.GetComponent<Text>().text;
+
+            if (TimerIsCountingDown)
+            {
+                if (TimerSecond == 0)
+                {
+                    //If the the second value reaches 0, count from 59 and reduce the minute value by 1
+                    TimerSecond = 59;
+                    --TimerMinute;
+
+                    //Update the new minute value in temptime
+                    if (TimerMinute <= 9)
+                    {
+                        temptime = '0' + TimerMinute.ToString() + temptime.Substring(2);
+                    }
+                    else
+                    {
+                        temptime = TimerMinute.ToString() + temptime.Substring(2);
+                    }
+                }
+                else
+                {
+                    //Minus 1 second in Second value
+                    --TimerSecond;
+                }
+            }
+            else
+            {
+                if(TimerSecond >= 59)
+                {
+                    TimerSecond = 0;
+                    ++TimerMinute;
+
+                    //Update the new minute value in temptime
+                    if (TimerMinute <= 9)
+                    {
+                        temptime = '0' + TimerMinute.ToString() + temptime.Substring(2);
+                    }
+                    else
+                    {
+                        temptime = TimerMinute.ToString() + temptime.Substring(2);
+                    }
+                }
+                else
+                {
+                    ++TimerSecond;
+                }
+            }
+
+            //Update the seconds value in temptime
+            if (TimerSecond <= 9)
+            {
+                temptime = temptime.Substring(0, 3) + '0' + TimerSecond.ToString();
+            }
+            else
+            {
+                temptime = temptime.Substring(0, 3) + TimerSecond.ToString();
+            }
+
+            //Assign the new time to the CanvasTimer
+            CanvasTimer.GetComponent<Text>().text = temptime;
+            //Reset the secondtimer
+            SecondTimer = 0;
+        }
+    }
 
     void SpawnAsteroid()
     {
