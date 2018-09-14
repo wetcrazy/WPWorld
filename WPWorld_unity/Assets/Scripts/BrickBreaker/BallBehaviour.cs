ff36164527265a3e44e4f1eff32ad792ba29729d
Ryan Lua <ryanlua5566@gmail.com>
2018-09-14 18:08:32 +0800

    Work for today Updating ball behaviour

--- WPWorld_unity/Assets/Scripts/BrickBreaker/BallBehaviour.cs
+++ WPWorld_unity/Assets/Scripts/BrickBreaker/BallBehaviour.cs
@@ -5,24 +5,70 @@ using UnityEngine;
 public class BallBehaviour : MonoBehaviour
 {
     public GameObject BrickPrefab;
+    public float speed, MAX_timer;
 
-    private GameObject[] arr_BrickOBJ;
-
+    private Vector3 oldPos;
+    private Vector3 vel;
+    private float curr_timer;
 
     private void Update()
     {
-        arr_BrickOBJ = GameObject.FindGameObjectsWithTag(BrickPrefab.tag);
+        // Check if the ball is moving
+        if (transform.position != oldPos)
+        {
+            oldPos = transform.position;
+            return;
+        }
+
+        Debug.Log("Ball: Time to move");
+        float _step = speed * Time.deltaTime;
+
+        if(curr_timer > MAX_timer)
+        {
+            curr_timer = 0.0f;
+            int _rng = Random.Range(0, 8);
+            switch(_rng)
+            {
+                case 0:
+                    vel = Vector3.forward;
+                    break;
+                case 1:
+                    vel = new Vector3(1.0f, 0.0f, 1.0f);
+                    break;
+                case 2:
+                    vel = Vector3.right;
+                    break;
+                case 3:
+                    vel = new Vector3(1.0f, 0.0f, -1.0f);
+                    break;
+                case 4:
+                    vel = -Vector3.forward;
+                    break;
+                case 5:
+                    vel = new Vector3(-1.0f, 0.0f, -1.0f);
+                    break;
+                case 6:
+                    vel = -Vector3.right;
+                    break;
+                case 7:
+                    vel = new Vector3(-1.0f, 0.0f, 1.0f);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        curr_timer += 0.1f;
+        transform.Translate(vel * _step);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision _collision)
     {
         Debug.Log("Ball Touched");
-        for (int i = 0; i < arr_BrickOBJ.Length; i++)
+        if (_collision.gameObject.tag == BrickPrefab.tag)
         {
-            if (collision.gameObject == arr_BrickOBJ[i])
-            {
-                Destroy(collision.gameObject);
-            }
+            Destroy(_collision.gameObject);
         }
 
     }
