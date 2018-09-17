e321be1a72a4b5ba0c522b6047d19178f7e49b70
CynicalSteve <syong789@hotmail.com>
2018-09-17 17:43:26 +0800

    -Added a virtual joystick -Unfinished joystick movement

---
+++ WPWorld_unity/Assets/Scripts/UI/Joystick.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Joystick : MonoBehaviour {
+
+    [SerializeField]
+    Button JoystickBall;
+    [SerializeField]
+    Image JoystickBackground;
+
+    public Vector3 JoystickDragDirection = Vector3.zero;
+    bool isDraggingJoystick = false;
+
+    Vector3 JoystickBackgroundPosition;
+    Vector3 Up = new Vector3(0, 100, 0);
+    public float FacingAngle;
+
+    public void OnJoystickDown()
+    {
+        isDraggingJoystick = true;
+    }
+
+    public void OnJoyStickUp()
+    {
+        isDraggingJoystick = false;
+        JoystickBall.transform.position = JoystickBackgroundPosition;
+        JoystickDragDirection = Vector3.zero;
+    }
+
+    private void Start()
+    {
+        JoystickBackgroundPosition = JoystickBackground.transform.position;
+    }
+
+    private void Update()
+    {
+        if(!isDraggingJoystick)
+        {
+            return;
+        }
+
+        if(Application.platform == RuntimePlatform.Android)
+        {
+        }
+        else if(Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            Vector3 MousePos = Input.mousePosition;
+            if (Vector3.Distance(MousePos, JoystickBackgroundPosition) < 100)
+            {
+                JoystickBall.transform.position = MousePos;
+            }
+            else
+            {
+                JoystickDragDirection = (MousePos - JoystickBackgroundPosition).normalized;
+                FacingAngle = -Vector3.SignedAngle(JoystickDragDirection, Up, JoystickBall.transform.forward);
+
+                JoystickBall.transform.position = JoystickBackgroundPosition + (Quaternion.AngleAxis(
+                    FacingAngle, 
+                    Vector3.forward) * Up);
+            }
+        }
+    }
+    
+}
