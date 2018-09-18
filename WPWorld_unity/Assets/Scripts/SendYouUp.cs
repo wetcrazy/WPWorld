32dbce94f630dfbbd464abe9e656995bccde186a
LilF <wilfredmaple@gmail.com>
2018-09-18 16:47:16 +0800

    Updated Stage, created Stage 1 prefabs, trap sends you upwards, and D pad textures.

---
+++ WPWorld_unity/Assets/Scripts/SendYouUp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendYouUp : MonoBehaviour {
+    public GameObject player;
+    public float speed;
+   
+    bool collided;
+	// Use this for initialization
+	void Start () {
+        collided = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(collided)
+        player.transform.Translate(0, speed * Time.deltaTime, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collided = true;
+            Debug.Log("HAHAHA.");
+        }
+    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.tag == "Player")
+    //    {
+    //        other.attachedRigidbody.useGravity = false;
+    //        collided = true;
+    //        Debug.Log("HAHAHAHHA");
+    //    }
+    //}
+}
