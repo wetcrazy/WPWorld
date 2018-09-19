13c64466077ea50484af32ef742eb36b7d419999
Ryan Lua <ryanlua5566@gmail.com>
2018-09-19 18:23:27 +0800

    Final push. Added jumper

---
+++ WPWorld_unity/Assets/Scripts/BrickBreaker/BBJump.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BBJump : MonoBehaviour
+{
+    public float jumpSpeed;
+
+    private Vector3 Dir;
+    private Rigidbody Rb;
+    private bool isGrounded = true;
+
+    private void Awake()
+    {
+        Rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Debug.Log("JUMPING");
+            Rb.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
+            isGrounded = false;
+        }
+#endif
+        //Debug.Log(isGrounded);
+    }
+
+    private void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+}
