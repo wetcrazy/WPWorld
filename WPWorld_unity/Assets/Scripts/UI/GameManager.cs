e0f966349b1f31c008c556e49120520f7230a700
Ryan Lua <ryanlua5566@gmail.com>
2018-09-20 16:19:35 +0800

    Finished up Break Bricker, Edited player movment cs

---
+++ WPWorld_unity/Assets/Scripts/UI/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject PlayerOBJ;
+
+    private void Awake()
+    {
+        PlayerOBJ = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void SendPlayerReset()
+    {
+        PlayerOBJ.SendMessage("ResetPlayer");
+    }
+
+    public void DeleteSelf()
+    {
+        Destroy(transform.parent.gameObject);
+    }
+
+}
