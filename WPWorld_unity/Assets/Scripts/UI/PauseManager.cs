dd7c0612c3f738b145513f40ff254699195a5c39
CynicalSteve <syong789@hotmail.com>
2018-09-25 14:17:21 +0800

    -Removed old joystick from prefabs -Added a pause menu -Added a pause manager

---
+++ WPWorld_unity/Assets/Scripts/UI/PauseManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour {
+
+    GameManager gameManager = null;
+
+    private void Start()
+    {
+            //gameManager =
+    }
+
+    public void PauseButtonDown()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+    public void SaveGame()
+    {
+        
+    }
+
+   public void QuitToMainMenu()
+    {
+
+    }
+}
