96d3029ff0c39717d123e9d25c9943ba934688b4
Ryan Lua <ryanlua5566@gmail.com>
2018-10-01 19:25:47 +0800

    Done up the base for dungeonsweeper

---
+++ WPWorld_unity/Assets/Scripts/MineSweeper/AnchorPoint.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorPoint : MonoBehaviour
+{
+    private bool is_Done { get; set; }
+    private float num_Bomb { get; set; }
+    private float total_BlockCount { get; set; }
+}
