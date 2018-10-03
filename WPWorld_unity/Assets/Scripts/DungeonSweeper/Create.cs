f4e7a0d1fd43107ad8bd51e275a77d888603e3e0
Ryan Lua <ryanlua5566@gmail.com>
2018-10-03 12:19:06 +0800

    Updated during lunch time

---
+++ WPWorld_unity/Assets/Scripts/DungeonSweeper/Create.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create : MonoBehaviour
+{
+    public GameObject Manager;
+
+    public void onCreateButtonDown()
+    {
+        Manager.SendMessage("Set_Anchorie", DungeonsweeperManager.AnchorPointType.ANCHOR_TWO);
+    }
+
+}
