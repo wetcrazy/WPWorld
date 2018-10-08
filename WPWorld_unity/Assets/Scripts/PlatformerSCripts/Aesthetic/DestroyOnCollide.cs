40b12f9ae448d877c956c17e223603182fcc8555
SIDM\163941B <lucifer967@hotmail.com>
2018-10-08 16:08:26 +0800

    Fixed Destroy On Collide blocks Changed how IsGrounded is detected by using all 4 directions Changed level

--- WPWorld_unity/Assets/Scripts/PlatformerSCripts/Aesthetic/DestroyOnCollide.cs
+++ WPWorld_unity/Assets/Scripts/PlatformerSCripts/Aesthetic/DestroyOnCollide.cs
@@ -36,27 +36,36 @@ public class DestroyOnCollide : MonoBehaviour {
     {
         GameObject CollidedObject = collision.gameObject;
 
-        if(CollidedObject.tag == "Player" && RenderRef.isVisible)
+        if(CollidedObject.tag == "Player")
         {
-            if (CollidedObject.transform.position.y + CollidedObject.transform.lossyScale.y / 2
-                <= transform.position.y - transform.lossyScale.y / 2 && Mathf.Abs(CollidedObject.transform.position.x - transform.position.x) < transform.lossyScale.x * 0.5f)
+            Debug.Log(CollidedObject.GetComponent<Rigidbody>().velocity);
+        }
+
+        if (CollidedObject.tag == "Player" // If Player
+            && RenderRef.isVisible // Check if the block is visible
+            && !CollidedObject.GetComponent<TPSLogic>().GetGrounded() // Checks if the player is grounded
+            && CollidedObject.GetComponent<Rigidbody>().velocity.y > 0) // Checks if the velocity of the player isn't falling
+        {
+            if (CollidedObject.transform.position.y + CollidedObject.transform.lossyScale.y / 2 <= transform.position.y - transform.lossyScale.y / 2
+                && Mathf.Abs(CollidedObject.transform.position.x - transform.position.x) < transform.lossyScale.x * 0.5f
+                && Mathf.Abs(CollidedObject.transform.position.z - transform.position.z) < transform.lossyScale.z * 0.5f)
             {
-                if (!CollidedObject.GetComponent<TPSLogic>().GetGrounded() && CollidedObject.GetComponent<Rigidbody>().velocity.y > 0)
+                RenderRef.enabled = false;
+
+                if (DestroySFX != null && GameObject.Find("Sound System") != null)
+                    GameObject.Find("Sound System").GetComponent<SoundSystem>().PlaySFX(DestroySFX);
+
+                for (int i = 0; i < AmountOfDebris; i++)
                 {
-                    RenderRef.enabled = false;
+                    GameObject n_Debris = Instantiate(Debris, this.transform);
+                    Rigidbody RigidRef = n_Debris.GetComponent<Rigidbody>();
+                    RigidRef.AddForce(new Vector3(Random.Range(-50, 50),
+                        Random.Range(25, 50),
+                        Random.Range(-50, 50)));
+                }
 
-                    if (DestroySFX != null && GameObject.Find("Sound System") != null)
-                        GameObject.Find("Sound System").GetComponent<SoundSystem>().PlaySFX(DestroySFX);
-
-                    for (int i = 0; i < AmountOfDebris; i++)
-                    {
-                        GameObject n_Debris = Instantiate(Debris, this.transform);
-                        Rigidbody RigidRef = n_Debris.GetComponent<Rigidbody>();
-                        RigidRef.AddForce(new Vector3(Random.Range(-50, 50),
-                            Random.Range(25, 50),
-                            Random.Range(-50, 50)));
-                    }
-
+                if(CollidedObject.GetComponent<Rigidbody>().velocity.y > 0)
+                {
                     Vector3 VelocityRef = CollidedObject.GetComponent<Rigidbody>().velocity;
                     VelocityRef.y = -VelocityRef.y * 0.5f;
                     CollidedObject.GetComponent<Rigidbody>().velocity = VelocityRef;
