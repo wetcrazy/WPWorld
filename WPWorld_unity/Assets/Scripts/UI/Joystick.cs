b578c8c3c70776b7067d57cafb23ef20f4383837
SIDM\163941B <lucifer967@hotmail.com>
2018-10-09 16:50:01 +0800

    Changed Joystick to work with the Multiplier feature

--- WPWorld_unity/Assets/Scripts/UI/Joystick.cs
+++ WPWorld_unity/Assets/Scripts/UI/Joystick.cs
@@ -196,8 +196,11 @@ public class Joystick : MonoBehaviour {
         }
 
         Vector4 DataPacket = new Vector4(DragDirection.x, DragDirection.y, DragDirection.z, (float)joystickDirection);
+        float Multiplier = Vector3.Distance(JoystickBall.transform.position, JoystickBackgroundPosition) / JoystickBallDragLengthLimit * 2;
 
         //Send the dragging direction and angle to the player
         PlayerObject.SendMessage("GetJoystickInput", DataPacket);
+
+        PlayerObject.SendMessage("SetMovementMultiplier", Multiplier);
     }
 }
