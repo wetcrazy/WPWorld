056e21c6c73afc44bbd4d8a37145d7dbd69f356e
Jkwaffle <janraphaelo1709@gmail.com>
2018-10-10 15:48:10 +0800

    update

--- WPWorld_unity/Assets/Scenes/Tetris/worksfromcom/blockspawner.cs
+++ WPWorld_unity/Assets/Scenes/Tetris/worksfromcom/blockspawner.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class blockspawner : MonoBehaviour {
    
     public GameObject aref;
-    int myrotation;//select which rotation
-    Vector3 a;
-    Vector3 b;
-    Vector3 c;
-    Vector3 d;
-    Vector3 e;
-    Vector3 f;
-    Vector3 g;
-    Vector3 h;
-    Vector3 i;
-    Vector3 j;
-    Vector3 k;
-    Vector3 l;
-    float scaleof;
+    public GameObject bref;
+    public GameObject cref;
+    public GameObject dref;
+    public GameObject eref;
+    public GameObject fref;
+    public GameObject gref;
+    public GameObject href;
+    public GameObject iref;
+    //blocks
+    List<GameObject> Blockrefs = new List<GameObject>();
+
+
+    //int myrotation;//select which rotation
+   
+    float scaleof;//0.1f
 
     bool Transnegx;
     bool Transposx;
@@ -26,26 +27,18 @@ public class blockspawner : MonoBehaviour {
     bool Transposz;
     Quaternion RotationOFF;
     
-    Vector3 temp;
+    
+    //rotations
     List<Vector3> HoliestGrail = new List<Vector3>();
     Vector3 myfinaltranslation;
+
     private void Start()
     {
         scaleof = 0.1f;
         ResetmyBool();
-         a = new Vector3(90f, 90f, 0f);     //1     1
-         b = new Vector3(90f, 0f, 90f);     //2       2
-         c = new Vector3(90f, 0f, 0f);      //3         3
-         d = new Vector3(90f, 0f, 180f);    //4           4
-         e = new Vector3(0f , 90f, 0f);      //5     1,2
-         f = new Vector3(0f , 90f, 180f);    //6     1,2
-         g = new Vector3(0f , 0f, 0f);       //7         3,4
-         h = new Vector3(0f , 0f, 180f);     //8         3,4
-         i = new Vector3(0f , 90f, 90f);     //9     1,2,3
-         j = new Vector3(0f , 90f, 270f);    //10    1,2,  4
-         k = new Vector3(0f , 0f, 270f);     //11    1,  3,4
-         l = new Vector3(0f , 0f, 90f);      //12      2,3,4
-        SetmyGrail();
+        Setfixedrotations();
+       // SetmyGrail();
+        SetmyGameobjectsRef();
         StartCoroutine(Example());
         
     }
@@ -53,28 +46,82 @@ public class blockspawner : MonoBehaviour {
     private void Update()
     {
         
-        Example();
+       
     }
 
-    IEnumerator Example()
+
+
+    void ResetmyBool()
     {
-        float gamex = Random.Range(-0.84f, 0.95f);
-        float gamez = Random.Range(-0.84f, 0.95f);
-        Vector3 currpos = this.gameObject.transform.position;
-        this.gameObject.transform.Translate(-currpos.x,0,-currpos.z,Space.World);
-        this.gameObject.transform.Translate(gamex, 0, gamez, Space.World);
-        //print(Time.time);
-        //setposnrot();
-        MoreRegret();
-        //Instantiate(a, this.gameObject.transform.position + OFFs,rotOFFS);
-        //Instantiate(a, this.gameObject.transform.position + OFFs,rotOFFS);
-       // Debug.Log("inexample");
-        yield return new WaitForSeconds(3);
-        // print(Time.time);
-        StartCoroutine(Example());
+        Transnegx = false;
+        Transposx = false;
+        Transnegz = false;
+        Transposz = false;
     }
-    void SetmyGrail()
-    {
+    void Setfixedrotations()
+    {                                                                                                                              
+        //                                        L ::   ::       ||  thumb   ::       ||   z      ::       ||    t    ::        ||      u    ::       ||    block
+        new Vector3( 0  , 0    , 0   );   //   345  ::   ::       ||  t 345   ::       ||  z  45   ::3      ||  t 345  ::        ||   u 345   ::       ||  b 345
+        new Vector3( 0  , 90   , 0   );   //   147  ::   ::       ||  t 147   ::       ||  z  47   ::1      ||  t 147  ::        ||   u 147   ::       ||  b 147
+        new Vector3( 0  , 180  , 0   );   //   345  ::   ::       ||  t 345   ::       ||  z  34   ::5      ||  t 345  ::        ||   u 345   ::       ||  b 345
+        new Vector3( 0  , 270  , 0   );   //   147  ::   ::       ||  t 147   ::       ||  z  14   ::7      ||  t 147  ::        ||   u 147   ::       ||  b 147
+                                                                                                                                                       
+        new Vector3( 90  , 0    , 0  );   //   0345 ::   ::       ||  t 01345 ::       ||  z  0145 ::       ||  t 1345 ::        ||   u 02345 ::       ||  b 012345
+        new Vector3( 90  , 90   , 0  );   //   1247 ::   ::       ||  t 12457 ::       ||  z  2457 ::       ||  t 1457 ::        ||   u 12478 ::       ||  b 124578
+        new Vector3( 90  , 180  , 0  );   //   3458 ::   ::       ||  t 34578 ::       ||  z  3478 ::       ||  t 3457 ::        ||   u 34568 ::       ||  b 345678
+        new Vector3( 90  , 270  , 0  );   //   1467 ::   ::       ||  t 13467 ::       ||  z  1346 ::       ||  t 1347 ::        ||   u 01467 ::       ||  b 013467
+                                                                                                                                                       
+        new Vector3( 180  , 0    , 0 );   //   3    ::45 ::       ||  t 34    ::5      ||  z  34   ::5      ||  t 4    ::35      ||   u 35    ::4      ||  b 345
+        new Vector3( 180  , 90   , 0 );   //   1    ::47 ::       ||  t 14    ::7      ||  z  14   ::7      ||  t 4    ::17      ||   u 17    ::4      ||  b 147
+        new Vector3( 180  , 180  , 0 );   //   5    ::45 ::       ||  t 45    ::3      ||  z  45   ::3      ||  t 4    ::35      ||   u 35    ::4      ||  b 345
+        new Vector3( 180  , 270  , 0 );   //   7    ::47 ::       ||  t 47    ::1      ||  z  47   ::1      ||  t 4    ::17      ||   u 17    ::4      ||  b 147
+                                                                                                                                                       
+        new Vector3( 270  , 0    , 0 );   //   3456 ::   ::       ||  t 34567 ::       ||  z 4567  ::       ||  t 3457 ::        ||   u 34568 ::       ||  b 345678
+        new Vector3( 270  , 90   , 0 );   //   0147 ::   ::       ||  t 01347 ::       ||  z 0347  ::       ||  t 1347 ::        ||   u 01467 ::       ||  b 013467
+        new Vector3( 270  , 180  , 0 );   //   2345 ::   ::       ||  t 12345 ::       ||  z 1234  ::       ||  t 1345 ::        ||   u 02345 ::       ||  b 012345
+        new Vector3( 270  , 270  , 0 );   //   1478 ::   ::       ||  t 14578 ::       ||  z 1458  ::       ||  t 1457 ::        ||   u 12478 ::       ||  b 124578
+                                                                                                                                                       
+        new Vector3( 0  , 0    , 90  );   //   34   ::   ::       ||  t 34    ::       ||  z 3     ::4      ||  t 4    ::3       ||   u 34    ::       ||  b 34
+        new Vector3( 0  , 90   , 90  );   //   14   ::   ::       ||  t 14    ::       ||  z 1     ::4      ||  t 4    ::1       ||   u 14    ::       ||  b 14
+        new Vector3( 0  , 180  , 90  );   //   45   ::   ::       ||  t 45    ::       ||  z 5     ::4      ||  t 4    ::5       ||   u 45    ::       ||  b 45
+        new Vector3( 0  , 270  , 90  );   //   47   ::   ::       ||  t 47    ::       ||  z 7     ::4      ||  t 4    ::7       ||   u 47    ::       ||  b 47
+                                                                                                                                                       
+        new Vector3( 0  , 0    , 270 );   //   4    ::   ::5      ||  t 4     ::5      ||  z 4     ::5      ||  t 4    ::5       ||   u 45    ::       ||  b 45
+        new Vector3( 0  , 90   , 270 );   //   4    ::   ::7      ||  t 4     ::7      ||  z 4     ::7      ||  t 4    ::7       ||   u 47    ::       ||  b 47
+        new Vector3( 0  , 180  , 270 );   //   4    ::   ::3      ||  t 4     ::3      ||  z 4     ::3      ||  t 4    ::3       ||   u 34    ::       ||  b 34
+        new Vector3( 0  , 270  , 270 );   //   4    ::   ::1      ||  t 4     ::1      ||  z 4     ::1      ||  t 4    ::1       ||   u 14    ::       ||  b 14 
+                                                                                                                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Vector3 a = new Vector3(90f, 90f, 0f);     //1     1
+       Vector3 b = new Vector3(90f, 0f, 90f);     //2       2
+       Vector3 c = new Vector3(90f, 0f, 0f);      //3         3
+       Vector3 d = new Vector3(90f, 0f, 180f);    //4           4
+       Vector3 e = new Vector3(0f, 90f, 0f);      //5     1,2
+       Vector3 f = new Vector3(0f, 90f, 180f);    //6     1,2
+       Vector3 g = new Vector3(0f, 0f, 0f);       //7         3,4
+       Vector3 h = new Vector3(0f, 0f, 180f);     //8         3,4
+       Vector3 i = new Vector3(0f, 90f, 90f);     //9     1,2,3
+       Vector3 j = new Vector3(0f, 90f, 270f);    //10    1,2,  4
+       Vector3 k = new Vector3(0f, 0f, 270f);     //11    1,  3,4
+       Vector3 l = new Vector3(0f, 0f, 90f);      //12      2,3,4
         HoliestGrail.Add(a);
         HoliestGrail.Add(b);
         HoliestGrail.Add(c);
@@ -87,12 +134,59 @@ public class blockspawner : MonoBehaviour {
         HoliestGrail.Add(j);
         HoliestGrail.Add(k);
         HoliestGrail.Add(l);
-
     }
+    //void SetmyGrail()
+    //{
+    //    HoliestGrail.Add(a);
+    //    HoliestGrail.Add(b);
+    //    HoliestGrail.Add(c);
+    //    HoliestGrail.Add(d);
+    //    HoliestGrail.Add(e);
+    //    HoliestGrail.Add(f);
+    //    HoliestGrail.Add(g);
+    //    HoliestGrail.Add(h);
+    //    HoliestGrail.Add(i);
+    //    HoliestGrail.Add(j);
+    //    HoliestGrail.Add(k);
+    //    HoliestGrail.Add(l);
 
+    //}
+    void SetmyGameobjectsRef()
+    {
+        Blockrefs.Add(aref);
+        Blockrefs.Add(bref);
+        Blockrefs.Add(cref);
+        Blockrefs.Add(dref);
+        Blockrefs.Add(eref);
+        Blockrefs.Add(fref);
+        Blockrefs.Add(gref);
+        Blockrefs.Add(href);
+        Blockrefs.Add(iref);
+    }
+    IEnumerator Example()
+    {
+        Wheretofall();
+      
+        MoreRegret();
+        //Instantiate(a, this.gameObject.transform.position + OFFs,rotOFFS);
+        //Instantiate(a, this.gameObject.transform.position + OFFs,rotOFFS);
+       // Debug.Log("inexample");
+      //  yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(2);
+        // print(Time.time);
+        StartCoroutine(Example());
+    }
+    void Wheretofall()
+    {
+        float gamex = Random.Range(-0.84f, 0.95f);
+        float gamez = Random.Range(-0.84f, 0.95f);
+        Vector3 currpos = this.gameObject.transform.position;
+        this.gameObject.transform.Translate(-currpos.x, 0, -currpos.z, Space.World);
+        this.gameObject.transform.Translate(gamex, 0, gamez, Space.World);
+    }
     void MoreRegret()
     {
-        myrotation = Random.Range(0, 12);
+        int myrotation = Random.Range(0, 12);
         switch (myrotation)
         {
         //Vector3 a = new Vector3(90, 90, 0);     //1     1
@@ -181,37 +275,20 @@ public class blockspawner : MonoBehaviour {
                 Transposz = true;
                 break;
         }
-        temp = HoliestGrail[myrotation];
+        Vector3 temp = HoliestGrail[myrotation];
         RotationOFF = Quaternion.Euler(temp);
         //Debug.Log("myrot   " + temp);
         Instantiatemyblock();
     }
 
-    void ResetmyBool()
-    {
-        Transnegx = false;
-        Transposx = false;
-        Transnegz = false;
-        Transposz = false;
-    }
-
     void Instantiatemyblock()
     {
+        int blocknum =  Random.Range( 0,9);
         SetmyTranslations();
-        float myposx = this.gameObject.transform.position.x;
-        float fixx;
-        fixx = Mathf.Round(this.gameObject.transform.position.x * 10f) / 10f;
-        float mynewposx = fixx - myposx;
-
-        float myposz = this.gameObject.transform.position.z;
-        float fixz;
-        fixz = Mathf.Round(this.gameObject.transform.position.z * 10f) / 10f;
-        float mynewposz = fixz - myposz;
-        this.gameObject.transform.Translate( mynewposx,0, mynewposz, Space.World);
+        Roundoffandsetmypos();
        // this.gameObject.transform.Translate(mynewpos, 0, 0, Space.World);
-        Instantiate(aref, this.gameObject.transform.position + myfinaltranslation, RotationOFF);
+        Instantiate(Blockrefs[blocknum], this.gameObject.transform.position + myfinaltranslation, RotationOFF);
     }
-
    
     void SetmyTranslations()
     {
@@ -280,5 +357,18 @@ public class blockspawner : MonoBehaviour {
 
 
         myfinaltranslation = tempx + tempz;
+    }
+    void Roundoffandsetmypos()
+    {
+        float myposx = this.gameObject.transform.position.x;
+        float fixx;
+        fixx = Mathf.Round(this.gameObject.transform.position.x * 10f) / 10f;
+        float mynewposx = fixx - myposx;
+
+        float myposz = this.gameObject.transform.position.z;
+        float fixz;
+        fixz = Mathf.Round(this.gameObject.transform.position.z * 10f) / 10f;
+        float mynewposz = fixz - myposz;
+        this.gameObject.transform.Translate(mynewposx, 0, mynewposz, Space.World);
     }
 }
