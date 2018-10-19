1a87d1640fde49f0bf1754d3f59639144792be7b
SIDM\163941B <lucifer967@hotmail.com>
2018-10-19 14:59:30 +0800

    Platform Scriptor

--- WPWorld_unity/Assets/Scripts/PlatformerSCripts/PlatformStageScript.cs
+++ WPWorld_unity/Assets/Scripts/PlatformerSCripts/PlatformStageScript.cs
@@ -9,19 +9,21 @@ public class PlatformStageScript : MonoBehaviour {
 
     private void Awake()
     {
-        transform.eulerAngles = Vector3.zero;
-
-        Vector3 NewPos = Camera.main.transform.position;
-        NewPos.y = transform.position.y;
-
-        transform.position = NewPos;
+        transform.position = new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.z);
     }
 
     private void Start()
     {
+        transform.position = new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.z);
+
         if (BGM != null && GameObject.Find("Sound System") != null)
         {
             GameObject.Find("Sound System").GetComponent<SoundSystem>().PlayBGM(BGM);
         }
+    }
+
+    private void Update()
+    {
+        transform.eulerAngles = Vector3.zero;
     }
 }
