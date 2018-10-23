a0df5a17475b2ca0df6fd65ee2b4c46a38ad1e89
AiLongDesktop <lucifer967@hotmail.com>
2018-10-24 02:01:23 +0800

    Indicator Script should be functioning, this will let the player know if the player is currently within view or not

--- WPWorld_unity/Assets/Scripts/PlatformerSCripts/Aesthetic/UI/IndicatorScript.cs
+++ WPWorld_unity/Assets/Scripts/PlatformerSCripts/Aesthetic/UI/IndicatorScript.cs
@@ -5,31 +5,62 @@ using UnityEngine.UI;
 
 public class IndicatorScript : MonoBehaviour {
 
-    private Image ImageRef;
+    [SerializeField]
+    private float Speed = 1;
 
-    private Vector3 ScreenMiddle;
+    private GameObject LookAtRef;
+
+    private RectTransform RectRef;
 
 	// Use this for initialization
 	void Start () {
-        ImageRef = GetComponent<Image>();
+        LookAtRef = new GameObject("Test");
 
-        ScreenMiddle = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        RectRef = GetComponent<RectTransform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 ScreenPoint = Camera.main.WorldToViewportPoint(GameObject.FindGameObjectWithTag("Player").transform.position);
-
-        ImageRef.enabled = false;
-        if (ScreenPoint.z < Camera.main.nearClipPlane)
+        LookAtRef.transform.position = new Vector3(ScreenPoint.x * Screen.width, ScreenPoint.y * Screen.height, 0);
+        transform.position = Vector3.Lerp(transform.position, LookAtRef.transform.position, Speed * Time.deltaTime);
+        if (ScreenPoint.x > 0 && ScreenPoint.x < 1 &&
+            ScreenPoint.y > 0 && ScreenPoint.y < 1)
+        {
+            transform.GetChild(0).GetComponent<Image>().enabled = false;
             return;
+        }
 
-        if (ScreenPoint.x > 0 && ScreenPoint.x < 1
-            && ScreenPoint.y > 0 && ScreenPoint.y < 1)
-            return;
+        transform.GetChild(0).GetComponent<Image>().enabled = true;
 
-        ImageRef.enabled = true;
+        transform.LookAt(LookAtRef.transform);
 
-        Vector3 TargetDir = new Vector3(ScreenPoint.x * ScreenMiddle.x, ScreenPoint.y * ScreenMiddle.y, 0);
+        if(RectRef.position.x < RectRef.sizeDelta.x / 2)
+        {
+            Vector3 n_Pos = RectRef.position;
+            n_Pos.x = RectRef.sizeDelta.x / 2;
+            RectRef.position = n_Pos;
+        }
+
+        if (RectRef.position.x > Screen.width - RectRef.sizeDelta.x / 2)
+        {
+            Vector3 n_Pos = RectRef.position;
+            n_Pos.x = Screen.width - RectRef.sizeDelta.x / 2;
+            RectRef.position = n_Pos;
+        }
+
+        if (RectRef.position.y < RectRef.sizeDelta.y / 2)
+        {
+            Vector3 n_Pos = RectRef.position;
+            n_Pos.y = RectRef.sizeDelta.y / 2;
+            RectRef.position = n_Pos;
+        }
+
+        if (RectRef.position.y > Screen.width - RectRef.sizeDelta.y / 2)
+        {
+            Vector3 n_Pos = RectRef.position;
+            n_Pos.y = Screen.width - RectRef.sizeDelta.y / 2;
+            RectRef.position = n_Pos;
+        }
     }
 }
