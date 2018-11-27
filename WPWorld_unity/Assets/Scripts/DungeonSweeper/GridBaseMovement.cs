2cfe203b0cb8f90a972a823f5d29badce7e3668a
Ryan Lua <ryanlua5566@gmail.com>
2018-11-27 18:04:46 +0800

    Added grids base movement

---
+++ WPWorld_unity/Assets/Scripts/DungeonSweeper/GridBaseMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBaseMovement : MonoBehaviour
+{
+    public GameObject blockObj;
+
+    [SerializeField]
+    private Vector3 respawnPoint;
+    [SerializeField]
+    private Vector3 movementDir;
+
+    private Rigidbody rb;
+    Joystick joystickControl;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
