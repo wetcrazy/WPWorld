b34681085c465c4e6a308a8bf27896612d05ac4f
Ryane <ryanlua5566@gmail.com>
2018-12-04 12:50:56 +0800

    Bomber man stuff is up but incomplete

---
+++ WPWorld_unity/Assets/Scripts/BomberMan/Bomb.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject BombFirePrefab;
+
+    private float currTimer;
+    private const float MAX_TIMER = 3.0f;
+    private int firePower;
+
+    private void Start()
+    {
+        currTimer = 0.0f;
+    }
+
+    private void Update()
+    {
+        if(currTimer > MAX_TIMER)
+        {
+            BlowUp();
+        }
+        else
+        {
+            currTimer += 1.0f * Time.deltaTime;
+        }
+    }
+
+    public void BlowUp()
+    {
+        
+        Destroy(this);
+    }
+
+    public void SetBombPower(int _newPower)
+    {
+        firePower = _newPower;
+    }
+}
