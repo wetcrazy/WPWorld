43d00e6b432495c905db933038d09398f38aa37d
Ryane <ryanlua5566@gmail.com>
2018-12-06 18:26:06 +0800

    Bomerfire fixing but spawning is out

---
+++ WPWorld_unity/Assets/Scripts/BomberMan/BombermanInputs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombermanInputs : MonoBehaviour
+{
+    private GameObject PlayerObj;
+
+    private void Start()
+    {
+        PlayerObj = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void OnBombButtonDown()
+    {
+        if(PlayerObj.GetComponent<BomberManPlayer>() == null)
+        {
+            Debug.Log("Error Spawn Bomb: Player not found");
+            return;
+        }
+        PlayerObj.SendMessage("SpawnBomb");
+    }
+}
