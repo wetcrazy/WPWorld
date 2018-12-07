91a639fa2b73e232f2a9c2fb18df1d58c659c82f
Ryane <ryanlua5566@gmail.com>
2018-12-07 18:41:18 +0800

    Fixed up the fire spawning

--- WPWorld_unity/Assets/Scripts/BomberMan/BomberManPlayer.cs
+++ WPWorld_unity/Assets/Scripts/BomberMan/BomberManPlayer.cs
@@ -8,18 +8,60 @@ public class BomberManPlayer : MonoBehaviour
 
     private int firePower;
     private int Lives;
+    private bool isDead;
+    private int MAX_NUMBOMB;
+    [SerializeField]
+    private int currNUMBomb;
+    private float currTimer;
+    private float MAX_TIMER;
+
+    private bool isLose;
 
     private void Start()
     {
+        isDead = false;
+        isLose = true;
         firePower = 10; // Default 1, can be increased
         Lives = 3;
+        currTimer = 0.0f;
+        MAX_TIMER = 3.0f;
+        MAX_NUMBOMB = 1;
+        currNUMBomb = 0;
+    }
+
+    private void Update()
+    {
+        if(isDead)
+        {
+            if(currTimer > MAX_TIMER)
+            {
+                currTimer = 0.0f;
+                if(Lives <= 0)
+                {
+                    isLose = true;
+                }
+                else
+                {
+                    Respawn();
+                }
+            }
+            else
+            {
+                currTimer += 1.0f * Time.deltaTime;
+            }
+        }
     }
 
     public void SpawnBomb()
     {
-        var newBomb = Bombprefab;
-        newBomb.GetComponent<Bomb>().SetBombPower(firePower);
-        Instantiate(newBomb, this.transform.position, Quaternion.identity, this.transform.parent);
+        if(isLose && currNUMBomb < MAX_NUMBOMB)
+        {
+            currNUMBomb += 1;
+            var newBomb = Bombprefab;
+            newBomb.GetComponent<Bomb>().SetBombPower(firePower);
+            newBomb.GetComponent<Bomb>().SetBombOwner(this.transform.gameObject);
+            Instantiate(newBomb, this.transform.position, Quaternion.identity, this.transform.parent);
+        }   
     }
 
     public void Respawn()
@@ -27,11 +69,13 @@ public class BomberManPlayer : MonoBehaviour
         this.transform.GetComponent<GridMovementScript>().Respawn();
     }
 
-    // Inputs
-    public void GetBombSpawnInput()
+    public void SetisDead(bool _boolvalue)
     {
-        SpawnBomb();
+        isDead = _boolvalue;
     }
 
-  
+    public void OneBombDestory()
+    {
+        currNUMBomb -= 1;
+    }
 }
---
+++ WPWorld_unity/Assets/Scripts/BomberMan/BombermanManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombermanManager : MonoBehaviour
+{
+    public void PlayerDead(GameObject _selectedOBJ, bool _boolValue)
+    {
+        _selectedOBJ.GetComponent<BomberManPlayer>().SetisDead(_boolValue);
+    }
+
+    public void DestoryMyBombCount(GameObject _selectedOBJ)
+    {
+        _selectedOBJ.GetComponent<BomberManPlayer>().OneBombDestory();
+    }
+}
