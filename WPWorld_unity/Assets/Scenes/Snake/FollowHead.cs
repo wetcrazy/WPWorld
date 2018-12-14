8d6e628b3e70099d4d230a71287b82e03c16b2be
JKSWAFFL3 <janraphaelo1709@gmail.com>
2018-12-14 14:17:46 +0800

    snake game demo working(snake fixed to 2 body and 1 head) extending body in progress

--- WPWorld_unity/Assets/Scenes/Snake/FollowHead.cs
+++ WPWorld_unity/Assets/Scenes/Snake/FollowHead.cs
@@ -4,58 +4,161 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class FollowHead : MonoBehaviour {
-    public GameObject P;
-    public GameObject C;
-    private GameObject Parent;
-    private GameObject Child;
+    //public GameObject P;
+    //   public GameObject C;
+    //  // private GameObject Parent;
+    //   private GameObject Child;
+    //   private GameObject newChild;
 
-    
-    public Text debugdistance;
-    public Text Headpos;
-    public Text Bodypos;
+    //   public Text debugdistance;
+    //   public Text Headpos;
+    //   public Text Bodypos;
 
-    public MyplayerScript huh;
+    //   public Text objectcondition;
+
+    //   private FollowHead Childfh;
+    //   private FollowHead newCHfh;
+    //   Vector3 temp;
+
+
+    //   Vector3 positionto;
+    //   // Use this for initialization
+    //   void Start () {
+    //      // SetParent(P);
+    //       SetChild(C);
+
+    //           //huh = Parent.GetComponent<MyplayerScript>();
+    //       Childfh = Child.GetComponent<FollowHead>();
+    //       newCHfh = newChild.GetComponent<FollowHead>();
+    //       positionto = new Vector3();
+    //       temp = new Vector3();
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //       objectcondition.text = this.gameObject.name + " at " + this.gameObject.transform.position.ToString();
+
+
+    //       Vector3 mag = new Vector3();
+    //     //  var m = huh.S_point;
+
+    //     //  mag = Parent.transform.position - m;
+    //     //  temp =  m-transform.position;
+
+    //       debugdistance.text = " distance:  "+ temp.ToString();
+    //       Bodypos.text = "BodyPos: " + transform.position.ToString();
+    //     //  Headpos.text = "HeadPos: " + Parent.transform.position.ToString();
+
+    //       // Vector3 offset = new Vector3(1, 0, 0);
+
+    //       if (mag.magnitude > 0.8f) 
+    //       {
+    //           transform.Translate(temp);
+    //       }
+    //}
+
+    //   //pop root child push new root child(this new root child's =old root child 
+    //   //and update
+    //   void Newchild(GameObject NEWCH)
+    //   {
+    //       if(this.gameObject.GetComponent<MyplayerScript>())
+    //       {
+    //           newCHfh.SetChild(Child);
+    //           SetChild(NEWCH);
+    //       }
+    //   }
+
+    //   void SetParent(GameObject P)
+    //   {
+    //       if (P)
+    //       {
+    //           //Parent = P;
+    //       }
+    //   }
+    //   void SetChild(GameObject C)
+    //   {
+    //       if (C)
+    //       {
+    //           Child = C;
+    //       }
+    //   }
+
+    public GameObject Child1;
+    public GameObject Parent1;
+    private GameObject C;
+    private GameObject P;
+
+    public int count;
+    public Text Pos1;
+    public Text Pos2;
+    Vector3 endpos;
+    Vector3 prevpos;
+
     Vector3 temp;
-    // Use this for initialization
-    void Start () {
-        SetParent(P);
-        SetChild(C);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Vector3 mag = new Vector3();
-        var m = huh.S_point;
-        
-        mag = Parent.transform.position - m;
-        temp =  m-transform.position;
-
-        debugdistance.text = " distance:  "+ temp.ToString();
-        Bodypos.text = "BodyPos: " + transform.position.ToString();
-        Headpos.text = "HeadPos: " + Parent.transform.position.ToString();
-        
-        // Vector3 offset = new Vector3(1, 0, 0);
-
-        if (mag.magnitude > 0.8f) 
-        {
-            transform.Translate(temp);
-        }
-	}
-
-
-
-    void SetParent(GameObject P)
+    Vector3 S_point;
+    public void SetMyChild(GameObject Child)
     {
-        if (P)
-        {
-            Parent = P;
-        }
+        C = Child;
     }
-    void SetChild(GameObject C)
+    public void SetMyParent(GameObject Parent)
     {
-        if (C)
+        P = Parent;
+    }
+     public void SetMyEndPos(Vector3 pos)
+    {
+        prevpos = endpos;
+        endpos = pos;
+        
+    }
+    public void Addcount()
+    {
+        count++;
+    }
+    private void Start()
+    {
+        SetMyChild(Child1);
+        SetMyParent(Parent1);
+        endpos = new Vector3(0, 0, 0);
+        
+    }
+
+    void SetpopPos()
+    {
+        SetRoundOffFix(transform.position);
+        S_point = this.transform.position;
+    }
+    //translates this object to the rounded off of its position
+    void SetRoundOffFix(Vector3 T)
+    {
+        Vector3 temp = RoundoffFix(T);
+        transform.Translate(temp - T);
+    }
+    //Round off given vector to int and returns the rounded off vector
+    Vector3 RoundoffFix(Vector3 T)
+    {
+        var posx = Mathf.RoundToInt(T.x);
+        var posy = Mathf.RoundToInt(T.y);
+        var posz = Mathf.RoundToInt(T.z);
+        Vector3 S = new Vector3(posx, posy, posz);
+        return S;
+    }
+
+    private void Update()
+    {
+        if (temp.magnitude >= 1f)
         {
-            Child = C;
+            SetpopPos();
+
         }
+        Pos1.text = " Prev pos: " + count.ToString();
+        Pos2.text = " End pos: " + endpos.ToString();
+        C.gameObject.GetComponent<FollowHead>().SetMyEndPos(S_point);
+        if (!this.gameObject.GetComponent<MyplayerScript>())
+        {
+            Vector3 Direction = endpos - this.gameObject.transform.position;
+            this.gameObject.transform.Translate(Direction);
+        }
+        temp = S_point - this.transform.position;
     }
 }
--- WPWorld_unity/Assets/Scenes/Snake/MyplayerScript.cs
+++ WPWorld_unity/Assets/Scenes/Snake/MyplayerScript.cs
@@ -4,125 +4,103 @@ using UnityEngine;
 using UnityEngine.UI;
 public class MyplayerScript : MonoBehaviour {
 
+    public Text debugX;
+    public Text debugZ;
+    public int gridsize;
+
+
+    bool spawn;
+    Vector3 temp;
+    private Vector3 S_point;
     float Speed;
     bool Horizontal;
-
     float FixX;
     float FixZ;
     int appleCount;
-    public Text debugX;
-    public Text debugZ;
-    public Text debugsnakelength;
-    public Text SpointDist;
 
-    bool spawn;
-    public Vector3 S_point;
-    Vector3 temp;
-
-
+   
     void Start () {
         spawn = false;
         appleCount = 0;
         Horizontal = false;
         Speed = 0.01f;
-
         SetpopPos();
     }
     
     
     void Update () {
-        if (temp.magnitude > 1f)
+
+        if (temp.magnitude >= 1f)
         {
             SetpopPos();
+            
         }
-        //detect button press
+        ////detect button press
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        //with the button press move in that direction
-        if ((temp.magnitude > 0.8f) && (temp.magnitude < 1.2f) || !spawn)
+
+        ////with the button press move in that direction
+        if ((temp.magnitude > 0.9f) && (temp.magnitude < 1f) || !spawn)
         {
-            if (x > 0 && !Horizontal)
-            {
-                FixX = Speed;
-                FixZ = 0;
-                Horizontal = true;
-                spawn = true;
-
-                SetpopPos();
-            }
-            else if (x < 0 && !Horizontal)
-            {
-                FixX = -Speed;
-                FixZ = 0;
-                Horizontal = true;
-                spawn = true;
-
-                SetpopPos();
-            }
-            else if (z > 0 && Horizontal)
-            {
-                FixZ = Speed;
-                FixX = 0;
-                Horizontal = false;
-                spawn = true;
-
-                SetpopPos();
-            }
-            else if (z < 0 && Horizontal)
-            {
-                FixZ = -Speed;
-                FixX = 0;
-                Horizontal = false;
-                spawn = true;
-
-                SetpopPos();
-            }
+             Direction(x, z);
         }
-        //move in that direction
+
+        ////move in that direction
         transform.Translate(FixX, 0, FixZ);
-       
         temp = S_point - this.transform.position;
         TExtDebugging();
     }
 
+    //Direction to translate
+    void Direction(float x, float z)
+    {
+        if (x > 0 && !Horizontal)
+        {
+            FixX = Speed;
+            FixZ = 0;
+            Horizontal = true;
+            spawn = true;
+            SetpopPos();
+        }
+        else if (x < 0 && !Horizontal)
+        {
+            FixX = -Speed;
+            FixZ = 0;
+            Horizontal = true;
+            spawn = true;
+            SetpopPos();
+        }
+        else if (z > 0 && Horizontal)
+        {
+            FixZ = Speed;
+            FixX = 0;
+            Horizontal = false;
+            spawn = true;
+            SetpopPos();
+        }
+        else if (z < 0 && Horizontal)
+        {
+            FixZ = -Speed;
+            FixX = 0;
+            Horizontal = false;
+            spawn = true;
+            SetpopPos();
+        }
+    }
 
-    //void Distance_checker()
-    //{
-    //}
-
-    //void Direction(float x,float z)
-    //{
-    //    if (x > 0 && !Horizontal)
-    //    {
-    //        FixX = Speed;
-    //        FixZ = 0;
-    //        Horizontal = true;
-    //    }
-    //    //move left
-    //    else if (x < 0 && !Horizontal)
-    //    {
-    //        FixX = -Speed;
-    //        FixZ = 0;
-    //        Horizontal = true;
-    //    }
-
-
-    //    else if (z > 0 && Horizontal)
-    //    {
-    //        FixZ = Speed;
-    //        FixX = 0;
-    //        Horizontal = false;
-    //    }
-    //    else if (z < 0 && Horizontal)
-    //    {
-    //        FixZ = -Speed;
-    //        FixX = 0;
-    //        Horizontal = false;
-
-    //    }
-
-    //}
-
+    //translates and set Spoint
+    void SetpopPos()
+    {
+        SetRoundOffFix(transform.position);
+        S_point = this.transform.position;
+    }
+    //translates this object to the rounded off of its position
+    void SetRoundOffFix(Vector3 T)
+    {
+        Vector3 temp = RoundoffFix(T);
+        transform.Translate(temp - T);
+    }
+    //Round off given vector to int and returns the rounded off vector
     Vector3 RoundoffFix(Vector3 T)
     {
         var posx = Mathf.RoundToInt(T.x);
@@ -131,28 +109,12 @@ public class MyplayerScript : MonoBehaviour {
         Vector3 S = new Vector3(posx, posy, posz);
         return S;
     }
-    void SetRoundOffFix(Vector3 T)
-    {
-        Vector3 temp = RoundoffFix(T);
-        transform.Translate(temp - T);
-    }
-
-    void SetpopPos()
-    {
-        SetRoundOffFix(transform.position);
-        S_point = this.transform.position;
-    }
+    //to display
     void TExtDebugging()
     {
         debugX.text = " x : " + FixX.ToString();
         debugZ.text = " z : " + FixZ.ToString();
-        debugsnakelength.text = " eaten apples: " + appleCount.ToString();
-        SpointDist.text = " distance : " + S_point.ToString() + " to object pos " + transform.position + " = " + temp.magnitude;
     }
-
-
-
-
     //get apple
     private void OnTriggerEnter(Collider other)
     {
