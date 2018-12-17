13e54bbdaff7b6e5dab411d2f7b5d49c11d46ce6
Ryane <ryanlua5566@gmail.com>
2018-12-17 18:55:45 +0800

    Fixed splashscreen, left scene changing

--- WPWorld_unity/Assets/Scripts/UI/Splashscreen Scene/SplashScreenManager.cs
+++ WPWorld_unity/Assets/Scripts/UI/Splashscreen Scene/SplashScreenManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SplashScreenManager : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class SplashScreenManager : MonoBehaviour
 
     private void Start()
     {
+        // Button
         Color SPButtonColor = SinglePlayerButton.image.color;
         Color MPButtonColor = MultiPlayerButton.image.color;
 
@@ -20,17 +22,56 @@ public class SplashScreenManager : MonoBehaviour
 
         SinglePlayerButton.image.color = SPButtonColor;
         MultiPlayerButton.image.color = MPButtonColor;
+
+        // Text
+        Color SPButtonTextColor = SinglePlayerButton.gameObject.GetComponentInChildren<Text>().color;
+        Color MPButtonTextColor = MultiPlayerButton.gameObject.GetComponentInChildren<Text>().color;
+
+        SPButtonTextColor.a = 0;
+        MPButtonTextColor.a = 0;
+
+        SinglePlayerButton.gameObject.GetComponentInChildren<Text>().color = SPButtonTextColor;
+        MultiPlayerButton.gameObject.GetComponentInChildren<Text>().color = MPButtonTextColor;
+    }
+
+    private void Update()
+    {
+
     }
 
     public void RevealButtons()
     {
+        float step = Time.deltaTime;
+
+        // Buttons
         Color SPButtonColor = SinglePlayerButton.image.color;
         Color MPButtonColor = MultiPlayerButton.image.color;
 
-        SPButtonColor.a = Mathf.Lerp(SPButtonColor.a, 1, 0.1f);
-        MPButtonColor.a = Mathf.Lerp(MPButtonColor.a, 1, 0.1f);
+        SPButtonColor.a = Mathf.Lerp(SPButtonColor.a, 1, step);
+        MPButtonColor.a = Mathf.Lerp(MPButtonColor.a, 1, step);
 
         SinglePlayerButton.image.color = SPButtonColor;
         MultiPlayerButton.image.color = MPButtonColor;
+
+
+        // Text
+        Color SPButtonTextColor = SinglePlayerButton.gameObject.GetComponentInChildren<Text>().color;
+        Color MPButtonTextColor = MultiPlayerButton.gameObject.GetComponentInChildren<Text>().color;
+
+        SPButtonTextColor.a = Mathf.Lerp(SPButtonTextColor.a, 1, step);
+        MPButtonTextColor.a = Mathf.Lerp(MPButtonTextColor.a, 1, step);
+
+        SinglePlayerButton.gameObject.GetComponentInChildren<Text>().color = SPButtonTextColor;
+        MultiPlayerButton.gameObject.GetComponentInChildren<Text>().color = MPButtonTextColor;
+    }
+
+    public void SinglePlayerButtonOnTouch()
+    {
+       
+    }
+
+    public void MultiPlayerButtonOnTouch()
+    {
+
     }
 }
