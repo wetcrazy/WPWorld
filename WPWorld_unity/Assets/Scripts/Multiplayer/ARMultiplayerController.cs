c0f3a8a4cdba1ecf7ef668c5f647709027d71dd7
CynicalSteve <syong789@hotmail.com>
2019-01-07 17:43:43 +0800

    no message

--- WPWorld_unity/Assets/Scripts/Multiplayer/ARMultiplayerController.cs
+++ WPWorld_unity/Assets/Scripts/Multiplayer/ARMultiplayerController.cs
@@ -250,6 +250,7 @@ public class ARMultiplayerController : MonoBehaviour
     private void SpawnLevel(Touch _touch)
     {
         _GroundObject = Instantiate(LevelObject, AnchorRef.transform.position, AnchorRef.transform.rotation, _anchor.transform);
+        Destroy(LevelObject);
     }
 
     // oooooooooooooooooooooooooooooooooooooooo
