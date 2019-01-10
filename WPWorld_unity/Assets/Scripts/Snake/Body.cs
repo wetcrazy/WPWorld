d98ee9b67e5d5c958d4ea7bc51f2e8c1aadf1d23
JKSWAFFL3 <janraphaelo1709@gmail.com>
2019-01-10 19:34:52 +0800

    Snake level

---
+++ WPWorld_unity/Assets/Scripts/Snake/Body.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : MonoBehaviour
+{
+    public Queue<Vector3> turningPos = new Queue<Vector3>();
+    public Queue<Head.STATE_FACING> turningDirection = new Queue<Head.STATE_FACING>();
+}
