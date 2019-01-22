25a8335336b6332e7545b103d3a521a9590aa4ed
Ryane <ryanlua5566@gmail.com>
2019-01-22 13:19:44 +0800

    Base class for powerup UP

---
+++ WPWorld_unity/Assets/Scripts/BomberMan/BombermanPowerupBase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombermanPowerupBase : MonoBehaviour
+{
+    private bool isCollected;
+  
+    // Constructor
+    public BombermanPowerupBase()
+    {
+        isCollected = false;
+    }
+
+    private void Update()
+    {
+        // Power up collected destory it
+        if(isCollected)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Constant Random Rotation
+    public void Rotate()
+    {
+        
+    }
+
+    // Empty Function
+    public virtual void Effect()
+    {
+
+    }
+
+    // Collision
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            isCollected = true;
+        }
+    }
+}
