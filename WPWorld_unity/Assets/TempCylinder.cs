e32eab9edd1e896c83870f2b744d1c657ad33999
CynicalSteve <syong789@hotmail.com>
2019-01-30 16:05:54 +0800

    no message

---
+++ WPWorld_unity/Assets/TempCylinder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempCylinder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.localPosition = ARMultiplayerController._GroundObject.transform.forward;
+	}
+}
